50d553d8753da78c82a7fe3fa59385824483f397
zhanglong <nice5good@126.com>
2018-08-20 17:03:36 +0800

    i添加自定义 创建控制器命令

--- resources/views/admin/errors/503.blade.php
+++ resources/views/admin/errors/503.blade.php
@@ -1,7 +1,33 @@
-@extends('layouts.web.master')
-@section('content')
-    <!-- 无客户显示 -->
-    <div id="none" style="display: block;">
-        <p>发生了错误</p>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> 对不起，服务发生了错误</title>
+    <meta name="renderer" content="webkit">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <link rel="stylesheet" href="http://www.layui.com/admin/std/dist/layuiadmin/style/admin.css" media="all">
+
+    <link rel="stylesheet" href="{{asset('/static/admin/font/css/font-awesome.min.css')}}">
+
+    <link rel="stylesheet" href="/static/admin/css/bootstrap.min.css">
+</head>
+<body>
+
+
+<div class="layui-fluid">
+    <div class="layadmin-tips">
+        <i class="fa fa-info-circle fa-5x " style="color:red"></i>
+        <div class="layui-text">
+            <h3>
+                对不起，服务发生了错误
+            </h3>
+            <br>
+
+        </div>
     </div>
-@endsection
+</div>
+
+</body>
+</html>
